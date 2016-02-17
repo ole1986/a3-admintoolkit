9b1703f3307ea467baf8846ee830442020f2432f
ole1986 <ole.k@web.de>
2016-02-17 16:08:18 +0100

    amended git hook allowing vscode to execute it through "Build" task (Ctrl+Shift+B)

--- githook-precommit.ps1
+++ githook-precommit.ps1
@@ -1,11 +1,22 @@
+[CmdletBinding()]
+Param(
+    [Switch]$Generate,
+    [Switch]$Sign,
+    [Switch]$Install
+)
+
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
-
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-$result = $host.ui.PromptForChoice("Confirm PBO generation", "Generate PBO files?", $options, 0)
+$steamPath = (Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Valve\Steam -Name SteamPath).SteamPath
+$AddonFolder = $steamPath + "\steamapps\common\Arma 3\@AdminToolkit"
 
 
-if($result -eq 0) {
+if(!$Generate) {
+    $result = $host.ui.PromptForChoice("Confirm PBO generation", "Generate PBO files?", $options, 0)
+}
+
+if($result -eq 0 -Or $Generate) {
     # CLIENT: use cpbo to (re)create the admintoolkit.pbo file
     cpbo -y -p .\@AdminToolkit\addons\admintoolkit .\@AdminToolkit\addons\admintoolkit.pbo
 
@@ -17,10 +28,17 @@ if($result -eq 0) {
     # SERVER: use cpbo to (re)create the admintoolkit.pbo file
     cpbo -y -p .\@ExileServer\admintoolkit .\@ExileServer\admintoolkit.pbo
 
-    $result = $host.ui.PromptForChoice("Confirm PBO signing", "Sign the Client PBO file?", $options, 0)
-    if($result -eq 0) {
+    if(!$Sign) { $result = $host.ui.PromptForChoice("Confirm PBO signing", "Sign the Client PBO file?", $options, 0) }
+
+    if($result -eq 0 -Or $Sign) {
         # CLIENT: build the bssign file for admintoolkit.pbo
         dssignfile .\admintoolkit.bikey .\@AdminToolkit\addons\admintoolkit.pbo   
+    }
+
+    if($Install) {
+        New-Item -path $AddonFolder\addons -type directory -Force | Out-Null
+        Copy-Item .\@AdminToolkit\mod.cpp -Destination $AddonFolder -Force
+        Copy-Item .\@AdminToolkit\addons\admintoolkit.* -Destination $AddonFolder\addons -Force
     }
 }
 
