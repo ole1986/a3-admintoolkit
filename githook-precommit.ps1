f0bf9df6ef307f2f8046015a4cd45c5bc0e690a2
ole <ole.k@web.de>
2016-02-17 22:19:01 +0100

    amended the powershell script for generating and signing the pbo files

--- githook-precommit.ps1
+++ githook-precommit.ps1
@@ -1,16 +1,23 @@
 [CmdletBinding()]
 Param(
+    [Parameter(Position=0)]
+    [string]$Path,
+    [Parameter(Position=1)]
+    [string]$Path2,
+    [Parameter(Position=2)]
+    [string]$Src,
     [Switch]$Generate,
     [Switch]$Sign,
     [Switch]$Install
 )
 
+$privateKeypath = ".\admintoolkit.biprivatekey"
+
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 $steamPath = (Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Valve\Steam -Name SteamPath).SteamPath
-$AddonFolder = $steamPath + "\steamapps\common\Arma 3\@AdminToolkit"
-
+$AddonFolder = $steamPath + "/steamapps/common/Arma 3/@AdminToolkit"
 
 if(!$Generate) {
     $result = $host.ui.PromptForChoice("Confirm PBO generation", "Generate PBO files?", $options, 0)
@@ -18,6 +25,7 @@ if(!$Generate) {
 
 if($result -eq 0 -Or $Generate) {
     # CLIENT: use cpbo to (re)create the admintoolkit.pbo file
+    Write-Host "--- Generate client PBO"
     cpbo -y -p .\@AdminToolkit\addons\admintoolkit .\@AdminToolkit\addons\admintoolkit.pbo
 
     # SERVER: replace sensitive password with an empty string in server config.cpp
@@ -26,19 +34,26 @@ if($result -eq 0 -Or $Generate) {
     Set-Content .\@ExileServer\admintoolkit\config.cpp -Value $content;
 
     # SERVER: use cpbo to (re)create the admintoolkit.pbo file
+    Write-Host "--- Generate server PBO"
     cpbo -y -p .\@ExileServer\admintoolkit .\@ExileServer\admintoolkit.pbo
 
     if(!$Sign) { $result = $host.ui.PromptForChoice("Confirm PBO signing", "Sign the Client PBO file?", $options, 0) }
 
     if($result -eq 0 -Or $Sign) {
         # CLIENT: build the bssign file for admintoolkit.pbo
-        dssignfile .\admintoolkit.bikey .\@AdminToolkit\addons\admintoolkit.pbo   
+        Write-Host "--- Signing client PBO"
+        dssignfile $privateKeypath .\@AdminToolkit\addons\admintoolkit.pbo
+        Write-Host "Done"
     }
 
-    if($Install) {
+    if(!$Install) { $result = $host.ui.PromptForChoice("Confirm Installation", "Install Addon into GameDir?", $options, 0) }
+
+    if($result -eq 0 -Or $Install) {
+        Write-Host "--- Installing client PBO (with bisign)"
         New-Item -path $AddonFolder\addons -type directory -Force | Out-Null
         Copy-Item .\@AdminToolkit\mod.cpp -Destination $AddonFolder -Force
         Copy-Item .\@AdminToolkit\addons\admintoolkit.* -Destination $AddonFolder\addons -Force
+        Write-Host "Done"
     }
 }
 
