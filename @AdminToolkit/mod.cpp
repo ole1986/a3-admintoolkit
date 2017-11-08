fb407b24af3bacfb10c31428be56f61628142272
ole <ole.k@web.de>
2017-11-08 20:11:42 +0100

    added author to arma-dev

--- @AdminToolkit/mod.cpp
+++ @AdminToolkit/mod.cpp
@@ -1,4 +1,4 @@
-name = "AdminToolkit";
+name = "Admin Toolkit [v2.2.1]";
 author = "ole1986";
-action = "http://github.com/ole1986/a3-admintoolkit";
-dir = "@AdminToolkit";
+actionName = "Website";
+action = "https://github.com/ole1986/a3-admintoolkit";
